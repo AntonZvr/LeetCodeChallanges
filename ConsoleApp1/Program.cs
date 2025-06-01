209459855fbcec597eb031492cd910f15b0683df
AntonZvr <mreclipsed@gmail.com>
2025-06-01 20:06:27 +0300

    [LongestCommonPrefix] solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+
+                if (prefix.Length == 0)
+                {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
