f3a289b7d6ff4807b7884105813c4f3eb7db0a1d
AntonZvr <mreclipsed@gmail.com>
2025-06-01 14:05:18 +0300

    [TwoSum] solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int sum = nums[i] + nums[j];
+                if (sum == target)
+                {
+                    int[] res = { Array.FindIndex(nums, x => x == nums[i]), Array.LastIndexOf(nums, nums[j]) };
+                    return res;
+                }
+            }
+        }
+        return new int[0];
+    }
+}
