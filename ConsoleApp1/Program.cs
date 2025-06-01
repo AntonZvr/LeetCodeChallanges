4ca4fbd596facc279cb00f29ad5adbee4152ad49
AntonZvr <mreclipsed@gmail.com>
2025-06-01 19:22:51 +0300

    [RomanToInt] solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+public class Solution
+{
+    public static int RomanToInt(string s)
+    {
+        Dictionary<char, int> romanToIntPairs = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        int res = 0;
+        int n = s.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            char currentChar = s[i];
+            char nextChar = s[i + 1];
+
+            int currentValue = romanToIntPairs[currentChar];
+            int nextValue = romanToIntPairs[nextChar];
+
+            if (currentValue < nextValue)
+            {
+                res -= currentValue;
+            }
+            else
+            {
+                res += currentValue;
+            }
+        }
+
+        res += romanToIntPairs[s[n - 1]];
+
+        return res;
+    }
+}
