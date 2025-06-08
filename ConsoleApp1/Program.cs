deb5f3044d163c34fbe542a374f99c07554e5af5
AntonZvr <mreclipsed@gmail.com>
2025-06-08 19:56:15 +0300

    [WordPattern]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        string[] words = s.Split(' ');
+
+        if (words.Length != pattern.Length)
+        {
+            return false;
+        }
+
+        Dictionary<char, string> charToStringMap = new Dictionary<char, string>();
+        Dictionary<string, char> stringToCharMap = new Dictionary<string, char>();
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            char currentChar = pattern[i];
+            string currentWord = words[i];
+
+            if (charToStringMap.ContainsKey(currentChar))
+            {
+                if (charToStringMap[currentChar] != currentWord)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                charToStringMap.Add(currentChar, currentWord);
+            }
+
+            if (stringToCharMap.ContainsKey(currentWord))
+            {
+                if (stringToCharMap[currentWord] != currentChar)
+                {
+                    return false; 
+                }
+            }
+            else
+            {
+                stringToCharMap.Add(currentWord, currentChar);
+            }
+        }
+
+        return true;
+    }
+}
