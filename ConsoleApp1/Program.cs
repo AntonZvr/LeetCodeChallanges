95f3f38c8d000313ccf43c25e8f88a0e9383bc84
AntonZvr <mreclipsed@gmail.com>
2025-06-08 18:46:22 +0300

    [RemoveDuplicates]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int RemoveDuplicatesUsingHashSet(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            return 0;
+        }
+
+        HashSet<int> seen = new HashSet<int>();
+        int k = 0;
+
+        foreach (int num in nums)
+        {
+            if (seen.Add(num))
+            {
+                nums[k] = num;
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
