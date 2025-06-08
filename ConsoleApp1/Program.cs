de5b63843092c143a92d90240e5bbf6c5210d20e
AntonZvr <mreclipsed@gmail.com>
2025-06-08 21:22:25 +0300

    [MergeSortedArray]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    // Output: [1,2,2,3,5,6]
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            nums1[m + i] = nums2[i];
+        }
+
+        Array.Sort(nums1);
+    }
+}
