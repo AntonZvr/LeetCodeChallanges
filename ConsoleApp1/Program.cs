71a6a7d8cee382466dc0a65718e1545455c6dd26
AntonZvr <mreclipsed@gmail.com>
2025-06-09 14:04:30 +0300

    [MajorityElement]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+        int mostFrequentNumber = nums[0];
+        int maxCount = 0;
+
+        foreach (int number in nums)
+        {
+            if (counts.ContainsKey(number))
+            {
+                counts[number]++;
+            }
+            else
+            {
+                counts[number] = 1;
+            }
+
+            if (counts[number] > maxCount)
+            {
+                maxCount = counts[number];
+                mostFrequentNumber = number;
+            }
+        }
+        return mostFrequentNumber;
+    }
+}
