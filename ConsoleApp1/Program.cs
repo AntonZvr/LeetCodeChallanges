d9e8466ada1240b7c1c31f90e0c3fe667ec301ba
AntonZvr <mreclipsed@gmail.com>
2025-06-09 15:29:18 +0300

    [TwoSum_II_Middle]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int sIndex = 0;
+        int lIndex = numbers.Length - 1;
+
+
+        while (sIndex < lIndex)
+        {
+            int sum = numbers[sIndex] + numbers[lIndex];
+
+            if (sum == target)
+            {
+                return new int[] { sIndex + 1, lIndex + 1 };
+            }
+            else if (sum < target)
+            {
+                sIndex++;
+            }
+            else
+            {
+                lIndex--;
+            }
+        }
+        return new int[0];
+    }
+}
