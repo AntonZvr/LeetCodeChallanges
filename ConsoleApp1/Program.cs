b5fd7444be99443150cc02ad8bbe65496af6a0c0
AntonZvr <mreclipsed@gmail.com>
2025-06-09 14:30:25 +0300

    [ReverseWordsMiddle]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string[] words = s.Split(new char[] { ' ', '.', ',', '!', '?', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+        StringBuilder res = new StringBuilder();
+        
+        Array.Reverse(words);
+
+        for (int i = 0; i < words.Length; i++) 
+        {
+            res.Append(words[i]);
+            if (i < words.Length - 1)
+            {
+                res.Append(' ');
+            }
+        }
+
+        return res.ToString();
+    }
+}
