44f72808b5053859ed36eee9ffb608481f1a09b1
AntonZvr <mreclipsed@gmail.com>
2025-06-10 21:17:10 +0300

    Comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,12 +2,15 @@
 {
     public int MajorityElement(int[] nums)
     {
+        // Dictionary to store counts of each number
         Dictionary<int, int> counts = new Dictionary<int, int>();
+
         int mostFrequentNumber = nums[0];
         int maxCount = 0;
 
         foreach (int number in nums)
         {
+            // Increment count if number already seen, else initialize to 1
             if (counts.ContainsKey(number))
             {
                 counts[number]++;
@@ -17,12 +20,14 @@
                 counts[number] = 1;
             }
 
+            // Update most frequent number if current count is higher
             if (counts[number] > maxCount)
             {
                 maxCount = counts[number];
                 mostFrequentNumber = number;
             }
         }
+
         return mostFrequentNumber;
     }
 }
