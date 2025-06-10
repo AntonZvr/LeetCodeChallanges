96c658c05448013a6c8841c2e678bdff4db869e5
AntonZvr <mreclipsed@gmail.com>
2025-06-10 21:07:55 +0300

    [MergeIntervals_Middle]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        if (intervals.Length <= 1)
+            return intervals;
+
+        // Sort intervals by start time
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+
+        List<int[]> merged = new List<int[]>();
+        int[] current = intervals[0];
+
+        foreach (var interval in intervals)
+        {
+            // If intervals overlap, merge them
+            if (interval[0] <= current[1])
+            {
+                current[1] = Math.Max(current[1], interval[1]);
+            }
+            else
+            {
+                merged.Add(current);
+                current = interval;
+            }
+        }
+
+        // Add the last interval
+        merged.Add(current);
+
+        return merged.ToArray();
+    }
+}
