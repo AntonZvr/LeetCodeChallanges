e4fafe185c5179589b483ede1fbb29af50b18836
AntonZvr <mreclipsed@gmail.com>
2025-06-10 20:58:13 +0300

    [GroupAnagrams_Middle]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        // Dictionary to group words by their sorted character form
+        Dictionary<string, List<string>> anagramGroups = new Dictionary<string, List<string>>();
+
+        foreach (string word in strs)
+        {
+            // Convert word to char array, sort it, and convert back to string
+            char[] chars = word.ToCharArray();
+            Array.Sort(chars);
+            string sorted = new string(chars);
+
+            // Use the sorted string as the key
+            if (!anagramGroups.ContainsKey(sorted))
+            {
+                anagramGroups[sorted] = new List<string>();
+            }
+
+            // Add the original word to the group
+            anagramGroups[sorted].Add(word);
+        }
+
+        // Return the groups as a list of lists
+        return new List<IList<string>>(anagramGroups.Values);
+    }
+}
