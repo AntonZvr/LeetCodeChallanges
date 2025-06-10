55d59b127e1553563b796417527cecab893dc3be
AntonZvr <mreclipsed@gmail.com>
2025-06-10 21:12:51 +0300

    Add comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ public class Solution
 {
     public static int RomanToInt(string s)
     {
+        // Map of Roman numerals to integer values
         Dictionary<char, int> romanToIntPairs = new Dictionary<char, int>
         {
             { 'I', 1 },
@@ -18,6 +19,7 @@ public class Solution
         int res = 0;
         int n = s.Length;
 
+        // Loop through the string, stopping at the second-to-last character
         for (int i = 0; i < n - 1; i++)
         {
             char currentChar = s[i];
@@ -26,6 +28,7 @@ public class Solution
             int currentValue = romanToIntPairs[currentChar];
             int nextValue = romanToIntPairs[nextChar];
 
+            // If the current value is less than the next, it's a subtractive pair
             if (currentValue < nextValue)
             {
                 res -= currentValue;
@@ -36,6 +39,7 @@ public class Solution
             }
         }
 
+        // Add the value of the last character
         res += romanToIntPairs[s[n - 1]];
 
         return res;
