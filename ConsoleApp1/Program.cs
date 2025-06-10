6ac5553d8a24f09d3d2f096d76e838326367468c
AntonZvr <mreclipsed@gmail.com>
2025-06-10 21:11:45 +0300

    Add comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,18 +2,29 @@
 {
     public int[] TwoSum(int[] nums, int target)
     {
+        // Loop through each element
         for (int i = 0; i < nums.Length; i++)
         {
+            // For each i, check all elements after it
             for (int j = i + 1; j < nums.Length; j++)
             {
                 int sum = nums[i] + nums[j];
+
+                // If a valid pair is found
                 if (sum == target)
                 {
-                    int[] res = { Array.FindIndex(nums, x => x == nums[i]), Array.LastIndexOf(nums, nums[j]) };
+                    // Find the first index of nums[i] and last index of nums[j]
+                    int[] res = {
+                        Array.FindIndex(nums, x => x == nums[i]),
+                        Array.LastIndexOf(nums, nums[j])
+                    };
+
                     return res;
                 }
             }
         }
+
+        // If no pair found, return empty array
         return new int[0];
     }
 }
