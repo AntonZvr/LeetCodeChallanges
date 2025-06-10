55346b17f785a76c3b2ae0580fc51e4edef9cbb5
AntonZvr <mreclipsed@gmail.com>
2025-06-10 21:14:22 +0300

    Add comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,14 +2,19 @@
 {
     public string LongestCommonPrefix(string[] strs)
     {
+        // Start with the first word as the initial prefix
         string prefix = strs[0];
 
+        // Compare the prefix with each subsequent string
         for (int i = 1; i < strs.Length; i++)
         {
+            // Shrink the prefix until the current word starts with it
             while (strs[i].IndexOf(prefix) != 0)
             {
+                // Remove the last character from the prefix
                 prefix = prefix.Substring(0, prefix.Length - 1);
 
+                // If the prefix becomes empty, there is no common prefix
                 if (prefix.Length == 0)
                 {
                     return "";
@@ -19,3 +24,4 @@
 
         return prefix;
     }
+}
