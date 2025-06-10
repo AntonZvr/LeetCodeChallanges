a5ac3b27942295fbae0b9cfcf7e144f990abf469
AntonZvr <mreclipsed@gmail.com>
2025-06-10 21:05:52 +0300

    [RotateArray_Middle]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        k = k % n; // In case k > n
+
+        // Reverse the whole array
+        Reverse(nums, 0, n - 1);
+
+        // Reverse first k elements
+        Reverse(nums, 0, k - 1);
+
+        // Reverse the rest
+        Reverse(nums, k, n - 1);
+    }
+
+    private void Reverse(int[] nums, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
